160809a074ced17a5c734f9b5ade0528cf1aa3cc
Gwangho <gh@relaybrand.com>
2021-03-30 17:46:45 +0900

    login

--- src/main/java/com/gh/rest/authentication/LoginResource.java
+++ src/main/java/com/gh/rest/authentication/LoginResource.java
@@ -1,9 +1,39 @@
 package com.gh.rest.authentication;
 
+import com.gh.config.security.CustomAuthenticationProvider;
+import com.gh.config.security.ShaPasswordEncoder;
+import com.gh.config.security.jwt.JwtResponse;
+import com.gh.config.security.jwt.JwtUtil;
+import com.gh.services.common.domain.User;
+import com.gh.services.users.domain.Users;
+import com.gh.services.users.service.UsersService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class LoginResource {
+
+    private final CustomAuthenticationProvider authenticationProvider ;
+    private final JwtUtil jwtUtil;
+
+    public LoginResource(CustomAuthenticationProvider authenticationProvider,
+                         JwtUtil jwtUtil,
+                         UsersService usersService,
+                         ShaPasswordEncoder passwordEncoder) {
+        this.authenticationProvider = authenticationProvider;
+        this.jwtUtil = jwtUtil;
+    }
+
 }
